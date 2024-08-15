3e31b9a952d0e45deca43e3e43201acc6805d59d
thiagohrcosta <thiagohrcosta86@gmail.com>
2024-08-15 18:36:08 -0300

    Adding styles

---
+++ src/styles/global.ts
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+
+    box-sizing: border-box;
+
+    :focus {
+      outline: 0;
+      box-shadow: 0 0 0 2px ${props => props.theme['gray-500']};
+    }
+  }
+
+
+`;
