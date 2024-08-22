6b61a5bae673bf98457c0e5e689b7b2dcd924cb4
thiagohrcosta <thiagohrcosta86@gmail.com>
2024-08-22 16:59:01 -0300

    Adding publication requests

--- src/components/Publications/index.tsx
+++ src/components/Publications/index.tsx
@@ -1,19 +1,33 @@
+import { useEffect, useState } from "react";
 import { Publication } from "../Publication";
 import { PublicationsComponentStyle } from "./style";
+import { api } from "../../services/api";
 
-interface PublicationsProps {
-  publications_number: number
-}
 
-export function Publications({ publications_number }: PublicationsProps) {
+export function Publications() {
+  const [searchParams, setSearchParams] = useState(null);
+  const [publicationsCounter, setPublicationsCounter] = useState(null)
+
+  async function handleFectchIssues() {
+    if (searchParams) {
+      const response = await api.get(`https://api.github.com/search/issues?q=${searchParams}:thiagohrcosta/Github-Blog`)
+      // setPublicationsCounter(response.data.total_count)
+      console.log("aqui", response.data)
+    }
+  }
+
+  useEffect(() => {
+    handleFectchIssues()
+  }, [searchParams, publicationsCounter])
+
   return (
     <PublicationsComponentStyle>
       <div className="publication-header">
         <h2>Publications</h2>
-        <p>{publications_number} publications</p>
+        <p>{publicationsCounter && publicationsCounter} publications</p>
       </div>
       <div className="search-publication-container">
-        <input type="text" placeholder="Search content" />
+        <input type="text" placeholder="Search content" onChange={(e) => setSearchParams(e.target.value)} />
       </div>
       <div className="publications-content-container">
         <Publication
@@ -37,7 +51,6 @@ export function Publications({ publications_number }: PublicationsProps) {
           date="19/08/2024"
         />
       </div>
-
     </PublicationsComponentStyle>
   )
 }
